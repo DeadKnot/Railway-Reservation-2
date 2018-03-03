c8f215d2a7601f16db76ab8092ca2319d42ff9c1
Mausam Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-03-03 17:48:24 +0530

    main function created
    RRS consists of 2 user types, one is admin and the other is normal passenger who wants to book a ticket on this system

--- railway.cpp
+++ railway.cpp
@@ -28,3 +28,36 @@ class login
 								cout<<"Password:";puts(pass);
 							}
 			};
+
+
+void main()
+	{
+		clrscr();
+		int ch;
+
+		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+		cout<<".......WELCOME TO RAILWAY RESERVATION SYSTEM..........\n";
+		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+
+		do
+		{
+			cout<<"^^^^^^^^^^^^^^^^^^^^^^MAIN MENU^^^^^^^^^^^^^^^^^^^^\n";
+			cout<<"1.Admin mode\n2.User mode\n3.Exit\n";
+			cout<<"Enter your choice:";
+			cin>>ch;
+			cout<<endl;
+
+
+			switch(ch)
+				{
+					case 1:			database(); break;
+					case 2:			user(); break;
+					case 3:			exit(0);
+
+				}
+
+		}while(ch<=3);
+
+
+		getch();
+	}
