629ff7a96d64f11e6f05342ee77d670213236697
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-05-06 14:15:00 +0530

    indentation corrected

--- railway.cpp
+++ railway.cpp
@@ -8,26 +8,22 @@
 #include<fstream.h>
 
 class login
-			{
-				public:
-					char id[15];
-					char pass[50];
-					char *password;
-						void getid()
-							{
-								cout<<"Enter your id:";gets(id);
-								password=getpass("Enter your password:");
-								
-								strcpy(pass,password);
-								
-							}
+{
+ public:
+	char id[15];
+	char pass[50];
+	char *password;
+	void getid(){
+		cout<<"Enter your id:";gets(id);
+		password=getpass("Enter your password:");
+		strcpy(pass,password);					
+	}
 
-						void displayid()
-							{
-								cout<<"Id:";puts(id);
-								cout<<"Password:";puts(pass);
-							}
-			};
+	void displayid(){
+		cout<<"Id:";puts(id);
+		cout<<"Password:";puts(pass);
+	}
+};
 
 
 void main()
