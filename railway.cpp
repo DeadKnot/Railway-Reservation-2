535aa3a11aec80632aab8fb0edbc78d5e90379fb
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-06-07 11:45:19 +0530

    structure going to rerdefined

--- railway.cpp
+++ railway.cpp
@@ -7,23 +7,7 @@
 #include<iomanip.h>
 #include<fstream.h>
 
-class login
-{
- public:
-	char id[15];
-	char pass[50];
-	char *password;
-	void getid(){
-		cout<<"Enter your id:";gets(id);
-		password=getpass("Enter your password:");
-		strcpy(pass,password);					
-	}
 
-	void displayid(){
-		cout<<"Id:";puts(id);
-		cout<<"Password:";puts(pass);
-	}
-};
 
 
 void main()
@@ -38,7 +22,7 @@ void main()
 		do
 		{
 			cout<<"^^^^^^^^^^^^^^^^^^^^^^MAIN MENU^^^^^^^^^^^^^^^^^^^^\n";
-			cout<<"1.Admin mode\n2.User mode\n3.Exit\n";
+			cout<<"1.Admin login\n2.User login\n3.Exit\n";
 			cout<<"Enter your choice:";
 			cin>>ch;
 			cout<<endl;
